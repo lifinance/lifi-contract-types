8f1ab99e1367b326d37e84eec72ed9602682a4f9
CallumGrindle <callumgrindle@gmail.com>
2024-11-23 17:03:18 +0000

    chore: release v2.50.4-beta

--- dist/index.js
+++ dist/index.js
@@ -14,7 +14,6 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
     Object.defineProperty(o, "default", { enumerable: true, value: v });
 }) : function(o, v) {
     o["default"] = v;
-});
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
